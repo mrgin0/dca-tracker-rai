69b2e4829805a1b0ea9fa178d1a5c245c4b84575
mrgin0 <97049921+mrgin0@users.noreply.github.com>
2026-07-20 10:55:16 +0800

    Change Firebase config strings to double quotes
    Updated Firebase configuration to use double quotes.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,12 +10,12 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebas
 
 // --- GANTI DENGAN KONFIG PROJECT FIREBASE LU ---
 const firebaseConfig = {
-  apiKey: 'AIzaSyDFyunVx6vAiFWKsvNU1izsoUdvz5tXCW0',
-  authDomain: 'family-office-3b102.firebaseapp.com',
-  projectId: 'family-office-3b102',
-  storageBucket: 'family-office-3b102.firebasestorage.app',
-  messagingSenderId: '1081122098957',
-  appId: '1:1081122098957:web:6517cf5ae17f1c63e46a44',
+  apiKey: "AIzaSyDFyunVx6vAiFWKsvNU1izsoUdvz5tXCW0",
+  authDomain: "family-office-3b102.firebaseapp.com",
+  projectId: "family-office-3b102",
+  storageBucket: "family-office-3b102.firebasestorage.app",
+  messagingSenderId: "1081122098957",
+  appId: "1:1081122098957:web:6517cf5ae17f1c63e46a44",
 };
 
 const app = initializeApp(firebaseConfig);
