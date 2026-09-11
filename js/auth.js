cff92969315dfc3e69088a1e9c7802a8e905147d
mrgin0 <97049921+mrgin0@users.noreply.github.com>
2026-09-11 11:00:29 +0800

    Update firebase-config import version in auth.js

--- js/auth.js
+++ js/auth.js
@@ -8,7 +8,7 @@ import {
   sendPasswordResetEmail,
   onAuthStateChanged,
 } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
-import { auth } from './firebase-config.js?v=10';
+import { auth } from './firebase-config.js?v=12';
 
 export function signIn(email, password) {
   return signInWithEmailAndPassword(auth, email, password);
